7276db33c76c685bad41dce9521f05cc3a71da02
chrisribia <chrisribia@gmail.com>
2019-10-08 12:34:19 +0300

    add register and login functionality

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for, flash, redirect
+from forms import LoginForm, RegistrationForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3ce72bb271c6adc396e4f8dd600610b0'
 
 posts = [
     {"author": "christopher ribia",
@@ -20,6 +23,27 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html',posts = posts)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
