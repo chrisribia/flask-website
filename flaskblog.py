ad66b15648836efbe050db43c86873962c4cc191
chrisribia <chrisribia@gmail.com>
2019-10-08 11:46:22 +0300

    add more

--- flaskblog.py
+++ flaskblog.py
@@ -4,20 +4,22 @@ app = Flask(__name__)
 posts = [
     {"author": "christopher ribia",
     "title":"corrutpion",
-    "Content": "Me without weed"},
+    "date_posted":"02/08/2001",
+    "content":"Me without weed"},
     {"author":"Busy signal",
+     "date_posted":"02/08/2001",
     "title":"Man a galis defender",
-    "Content":"Nice flow and vocals"
+    "content":"Nice flow and vocals"
     }
 ]
 
 @app.route("/")
 def hello():
-    return "domie wairimu!! chris loves you so much busy signal "
+    return render_template('home.html',posts = posts)
 
 @app.route("/about")
 def about():
-    return render_template('home.html',posts = posts)
+    return render_template('about.html',posts = posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
