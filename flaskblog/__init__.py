abc7f856f95ba039e65454a6427bcd4afea7342e
chrisribia <chrisribia@gmail.com>
2019-10-09 12:26:31 +0300

    add account html

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manage = LoginManager(app)
+login_manage.login_view = 'login'
+
+login_manage.login_message_category = 'danger'
 
 from flaskblog import routes
