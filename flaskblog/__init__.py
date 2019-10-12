4a61b21d230d78e3134b6886f9094df4e0031fbf
chrisribia <chrisribia@gmail.com>
2019-10-12 12:24:49 +0300

    email

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -10,7 +11,13 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manage = LoginManager(app)
 login_manage.login_view = 'login'
-
 login_manage.login_message_category = 'danger'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "thebigribia@gmail.com"
+app.config['MAIL_PASSWORD'] = "ribia100"
+mail = Mail(app)
 
 from flaskblog import routes
